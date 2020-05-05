4970706762c131fef9c46ed3ad5c8b0e6c444264
tfunk2 <tyler.j.funk2@gmail.com>
2020-05-05 02:24:36 -0600

    add validations to username and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :games
+
+    validates :username, :password, presence: true
+    validates :username, length: { minimum: 6, maximum: 16, message: "Username must be between 6 and 16 characters long."}
+    validates :password, length: { minimum: 6, maximum: 16, message: "Password must be between 6 and 16 characters long."}
+    validates :username, uniqueness: {message: "%{value} has already been taken."}
 end
